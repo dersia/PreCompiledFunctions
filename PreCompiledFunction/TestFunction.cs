cb457e68cf35cec5ef0b617ca2784989f06b73f3
Siavash Ghassemi <dersia@web.de>
2018-04-28 15:02:01 +0200

    initial commit

---
+++ PreCompiledFunction/TestFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace PreCompiledFunction
+{
+    public class TestFunction
+    {
+        [FunctionName("CoolStuff")]
+        public static HttpResponseMessage CoolStuff(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "CoolStuff")] HttpRequestMessage req,
+            ILogger log)
+        {
+            return req.CreateResponse(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
